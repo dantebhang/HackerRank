2d32e8ea5850bde5a8df8faa494b432749658750
bhangdante@gmail.com <87205105+dantebhang@users.noreply.github.com>
2022-01-20 07:55:21 -0800

    mini max sum

---
+++ Mini-MaxSum.js
@@ -0,0 +1,115 @@
+// Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+// Example
+// The minimum sum is and the maximum sum is
+
+// . The function prints
+
+// 16 24
+
+// Function Description
+
+// Complete the miniMaxSum function in the editor below.
+
+// miniMaxSum has the following parameter(s):
+
+//     arr: an array of 
+
+//     integers
+
+// Print
+
+// Print two space-separated integers on one line: the minimum sum and the maximum sum of
+// of
+
+// elements.
+
+// Input Format
+
+// A single line of five space-separated integers.
+
+// Constraints
+
+// Output Format
+
+// Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
+
+// Sample Input
+
+// 1 2 3 4 5
+
+// Sample Output
+
+// 10 14
+
+// Explanation
+
+// The numbers are
+// , , , , and
+
+// . Calculate the following sums using four of the five integers:
+
+//     Sum everything except 
+
+// , the sum is
+// .
+// Sum everything except
+// , the sum is
+// .
+// Sum everything except
+// , the sum is
+// .
+// Sum everything except
+// , the sum is
+// .
+// Sum everything except
+// , the sum is
+
+//     .
+
+// Hints: Beware of integer overflow! Use 64-bit Integer.
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+    
+    const minSum = (arr.reduce((prev, curr) => prev + curr) - max)
+    const maxSum = (arr.reduce((prev, curr) => prev + curr) - min)
+    
+    console.log(minSum + " " + maxSum)
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
