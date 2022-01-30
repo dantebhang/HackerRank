a1ef67f980a791c3ff818fa44807a8e0d9ffdced
bhangdante@gmail.com <87205105+dantebhang@users.noreply.github.com>
2022-01-30 08:01:48 -0800

    counting valleys

---
+++ week2/CountingValleys.js
@@ -0,0 +1,19 @@
+function countingValleys(steps, path) {
+  let step = 0;
+  let valleyCount = 0;
+  
+  path.split('').map((el) => {
+      if(el === 'U'){
+          step++
+      }
+      if(el === 'D'){
+          if(step === 0){
+              valleyCount++
+          }
+          step--
+      }
+  })
+  
+  return valleyCount;
+
+}
